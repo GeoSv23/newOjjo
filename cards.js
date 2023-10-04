ae4b8e5554f11803b0034d2e315a9f32f8523c0d
GeoSv23 <115006657+GeoSv23@users.noreply.github.com>
2023-10-04 22:42:25 +0300

    pop

--- cards.js
+++ cards.js
@@ -1,38 +1,35 @@
-// burger 
-const buttonBurger = document.querySelector('.burger');
+// burger
+const buttonBurger = document.querySelector(".burger");
 
-const burgerMenu = document.querySelector('.burgerMenu');
+const burgerMenu = document.querySelector(".burgerMenu");
 
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 
-const headerLogo = document.querySelector('.header__image');
+const headerLogo = document.querySelector(".header__image");
 
-buttonBurger.addEventListener('click', function() {
-  burgerMenu.classList.toggle('burgerMenuOpen')
-  buttonBurger.classList.toggle('burgerActive')
-  body.classList.toggle('antiscroll')
-  headerLogo.classList.toggle('logoBurger')
-}
-)
+buttonBurger.addEventListener("click", function () {
+  burgerMenu.classList.toggle("burgerMenuOpen");
+  buttonBurger.classList.toggle("burgerActive");
+  body.classList.toggle("antiscroll");
+  headerLogo.classList.toggle("logoBurger");
+});
 
-// 
-const socMedImg = document.querySelector('.socialMedias__images');
+//
+const socMedImg = document.querySelector(".socialMedias__images");
 
 const socMedAttributes = [
-  {link: '#', imgSrc: 'public/images/img--hover.png', alt: 'image'},
-  {link: '#', imgSrc: 'public/images/gallery__image.png', alt: 'image'},
-  {link: '#', imgSrc: 'public/images/gallery__image (1).png', alt: 'image'},
-  {link: '#', imgSrc: 'public/images/gallery__image (2).png', alt: 'image'},
-  {link: '#', imgSrc: 'public/images/gallery__image (3).png', alt: 'image'},
-  {link: '#', imgSrc: 'public/images/Rectangle 8.png', alt: 'image'},
-]
+  { link: "#", imgSrc: "public/images/img--hover.png", alt: "image" },
+  { link: "#", imgSrc: "public/images/gallery__image.png", alt: "image" },
+  { link: "#", imgSrc: "public/images/gallery__image (1).png", alt: "image" },
+  { link: "#", imgSrc: "public/images/gallery__image (2).png", alt: "image" },
+  { link: "#", imgSrc: "public/images/gallery__image (3).png", alt: "image" },
+  { link: "#", imgSrc: "public/images/Rectangle 8.png", alt: "image" },
+];
 
 socMedAttributes.forEach((item) => {
-  const newImageContain = document.createElement('a');
-  newImageContain.classList.add('.socialMedias__images_container');
-})
-
-
+  const newImageContain = document.createElement("a");
+  newImageContain.classList.add(".socialMedias__images_container");
+});
 
 // отображение кнопок product
 const product__buttons = document.querySelector(".product__buttons");
@@ -60,31 +57,31 @@ const productImgHolder = document.querySelector(".product__images");
 
 const productCards = [
   {
-    cardLink: "1",
+    cardLink: "#",
     imgSrc: "images/colco.png",
     title: "кольца",
   },
   {
-    cardLink: "2",
+    cardLink: "#",
     imgSrc: "images/colco.png",
     title: "серьги",
   },
   {
-    cardLink: "3",
+    cardLink: "#",
     imgSrc: "images/colco.png",
     title: "подвески",
   },
   {
-    cardLink: "4",
+    cardLink: "#",
     imgSrc: "images/colco.png",
     title: "запонки",
   },
   {
-    cardLink: "5",
+    cardLink: "#",
     imgSrc: "images/colco.png",
     title: "браслеты",
   },
-  { cardLink: "6", imgSrc: "public/images/Rectangle 5.png", title: "часы" },
+  { cardLink: "#", imgSrc: "images/colco.png", title: "часы" },
 ];
 
 productCards.forEach((object) => {
@@ -130,5 +127,3 @@ artCards.forEach((item) => {
 
   artImgHolder.innerHTML += newArtCard;
 });
-
-
